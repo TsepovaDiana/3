9524afa8c86eb4151fedfd7a670d9106846f4d1e
TsepovaDiana <tsepova.dina@gmail.com>
2024-11-25 18:15:02 +0400

    index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 </head>
 <body>
     <h1>Заполните форму</h1>
-    <form action="" method="POST">
+    <form action="process.php" method="POST">
+
         <label for="text">ФИО:</label>
         <input type="text" id="text" name="text" required><br><br>
 
@@ -33,94 +34,32 @@
         <label for="radio2">Мужской</label><br><br>
 
         <label>Ваша профессия:</label><br>
-        <input type="checkbox" id="checkbox" name="checkbox" value="1">
-        <label for="checkbox">студент</label><br><br>
-        <input type="checkbox" id="checkbox" name="checkbox" value="2">
-        <label for="checkbox">учитель</label><br><br>
-        <input type="checkbox" id="checkbox" name="checkbox" value="3">
-        <label for="checkbox">программист</label><br><br>
-        <input type="checkbox" id="checkbox" name="checkbox" value="4">
-        <label for="checkbox">экономист</label><br><br>
+        <input type="checkbox" id="checkbox1" name="checkbox[]" value="1">
+        <label for="checkbox1">студент</label><br>
+        <input type="checkbox" id="checkbox2" name="checkbox[]" value="2">
+        <label for="checkbox2">учитель</label><br>
+        <input type="checkbox" id="checkbox3" name="checkbox[]" value="3">
+        <label for="checkbox3">программист</label><br>
+        <input type="checkbox" id="checkbox4" name="checkbox[]" value="4">
+        <label for="checkbox4">экономист</label><br><br>
 
         <label for="password">Пароль:</label>
         <input type="password" id="password" name="password" required><br><br>
 
         <input type="submit" value="Отправить">
     </form>
+
+    <?php
+    if (isset($_GET['errors'])) {
+        $errors = json_decode($_GET['errors'], true);
+        if ($errors) {
+            echo "<ul style='color: red;'>";
+            foreach ($errors as $error) {
+                echo "<li>$error</li>";
+            }
+            echo "</ul>";
+        }
+    }
+    ?>
 </body>
 </html>
-
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $errors = [];
-    $data = [];
-
-    if (empty($_POST['text'])) {
-        $errors['text'] = "Введите ФИО";
-    } else {
-        $data['text'] = htmlspecialchars($_POST['text']);
-    }
-
-    if (empty($_POST['email'])) {
-        $errors['email'] = "Эмейл не может быть пустым";
-    } else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = "Неверный формат эмейла";
-    } else {
-        $data['email'] = htmlspecialchars($_POST['email']);
-    }
-
-    if (empty($_POST['number'])) 
-    {
-        $errors['number'] = "Введите возраст";
-    } 
-    else 
-    {
-        $data['number'] = htmlspecialchars($_POST['number']);
-    }
-
-    if (empty($_POST['select'])) 
-    {
-        $errors['select'] = "Выберите страну из списка";
-    } 
-    else 
-    {
-        $data['select'] = htmlspecialchars($_POST['select']);
-    }
-
-    if (empty($_POST['radio'])) 
-    {
-        $errors['radio'] = "Выберите пол";
-    } 
-    else 
-    {
-        $data['radio'] = htmlspecialchars($_POST['radio']);
-    }
-
-
-    $data['checkbox'] = isset($_POST['checkbox']) ? 1 : 0;
-
-    if (empty($_POST['password'])) 
-    {
-        $errors['password'] = "Пароль не может быть пустым";
-    } 
-    else 
-    {
-        $data['password'] = htmlspecialchars($_POST['password']);
-    }
-
-    if ($errors) 
-    {
-        foreach ($errors as $error) 
-        {
-            echo "<p style='color:red;'>$error</p>";
-        }
-    } 
-    
-    else 
-    {
-        $data['timestamp'] = date("Y-m-d H:i:s");
-        file_put_contents('log.json', json_encode($data, JSON_PRETTY_PRINT) . PHP_EOL, FILE_APPEND);
-        echo "<p style='color:green;'>Данные успешно записаны!</p>";
-    }
-}
-?>
